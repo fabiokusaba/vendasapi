96f25dca402dbd2c0939ae17addad0b806c5dce0
Fabio Kusaba <fabinhokb44@gmail.com>
2025-04-06 10:14:53 -0300

    feat: adicionar método de conversão de tipo entity para response

--- src/main/java/com/fabiokusaba/vendas_api/api/v1/response/CadastrarClienteResponse.java
+++ src/main/java/com/fabiokusaba/vendas_api/api/v1/response/CadastrarClienteResponse.java
@@ -1,9 +1,20 @@
 package com.fabiokusaba.vendas_api.api.v1.response;
 
+import com.fabiokusaba.vendas_api.domain.model.Cliente;
+
 public record CadastrarClienteResponse(
         String id,
         String nome,
         String cpf,
         EnderecoResponse endereco
 ) {
+
+    public static CadastrarClienteResponse fromModel(Cliente cliente) {
+        return new CadastrarClienteResponse(
+                cliente.getId(),
+                cliente.getNome(),
+                cliente.getCpf(),
+                EnderecoResponse.toModel(cliente)
+        );
+    }
 }
--- src/main/java/com/fabiokusaba/vendas_api/api/v1/response/EnderecoResponse.java
+++ src/main/java/com/fabiokusaba/vendas_api/api/v1/response/EnderecoResponse.java
@@ -1,5 +1,8 @@
 package com.fabiokusaba.vendas_api.api.v1.response;
 
+import com.fabiokusaba.vendas_api.domain.model.Cliente;
+import com.fabiokusaba.vendas_api.domain.vo.Endereco;
+
 public record EnderecoResponse(
         String cep,
         String uf,
@@ -9,4 +12,16 @@ public record EnderecoResponse(
         String complemento,
         String numero
 ) {
+
+    public static EnderecoResponse toModel(Cliente cliente) {
+        return new EnderecoResponse(
+                cliente.getEndereco().getCep(),
+                cliente.getEndereco().getUf(),
+                cliente.getEndereco().getCidade(),
+                cliente.getEndereco().getBairro(),
+                cliente.getEndereco().getLogradouro(),
+                cliente.getEndereco().getComplemento(),
+                cliente.getEndereco().getNumero()
+        );
+    }
 }
