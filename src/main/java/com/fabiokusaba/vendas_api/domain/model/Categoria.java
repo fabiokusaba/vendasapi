4f47e5f0860e2fde88d7943b14a00e1986ceb572
Fabio Kusaba <fabinhokb44@gmail.com>
2025-04-06 16:57:08 -0300

    feat: adicionar model e repository de categoria

---
+++ src/main/java/com/fabiokusaba/vendas_api/domain/model/Categoria.java
@@ -0,0 +1,38 @@
+package com.fabiokusaba.vendas_api.domain.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Document(collection = "categoria")
+public class Categoria {
+
+    @Id
+    private String id;
+    private String nome;
+    private String descricao;
+
+    private Categoria(String id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public static Categoria novaCategoria(String nome, String descricao) {
+        final var id = UUID.randomUUID().toString();
+        return new Categoria(id, nome, descricao);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
