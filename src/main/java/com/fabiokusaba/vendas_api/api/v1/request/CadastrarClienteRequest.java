93ce95fd23dd54ac942001fe0032256870c7fc2d
Fabio Kusaba <fabinhokb44@gmail.com>
2025-04-06 10:07:11 -0300

    feat: adicionar método de conversão de tipo request para entity

--- src/main/java/com/fabiokusaba/vendas_api/api/v1/request/CadastrarClienteRequest.java
+++ src/main/java/com/fabiokusaba/vendas_api/api/v1/request/CadastrarClienteRequest.java
@@ -1,8 +1,18 @@
 package com.fabiokusaba.vendas_api.api.v1.request;
 
+import com.fabiokusaba.vendas_api.domain.model.Cliente;
+
 public record CadastrarClienteRequest(
         String nome,
         String cpf,
         EnderecoRequest endereco
 ) {
+
+    public static Cliente toModel(CadastrarClienteRequest request) {
+        return Cliente.novoCliente(
+                request.nome(),
+                request.cpf(),
+                EnderecoRequest.toModel(request.endereco())
+        );
+    }
 }
--- src/main/java/com/fabiokusaba/vendas_api/api/v1/request/EnderecoRequest.java
+++ src/main/java/com/fabiokusaba/vendas_api/api/v1/request/EnderecoRequest.java
@@ -1,5 +1,7 @@
 package com.fabiokusaba.vendas_api.api.v1.request;
 
+import com.fabiokusaba.vendas_api.domain.vo.Endereco;
+
 public record EnderecoRequest(
         String cep,
         String uf,
@@ -9,4 +11,16 @@ public record EnderecoRequest(
         String complemento,
         String numero
 ) {
+
+    public static Endereco toModel(EnderecoRequest request) {
+        return Endereco.novoEndereco(
+                request.cep(),
+                request.uf(),
+                request.cidade(),
+                request.bairro(),
+                request.logradouro(),
+                request.complemento(),
+                request.numero()
+        );
+    }
 }
