f27c11ce5d38030dbea7a3e0064222eb38294771
Fabio Kusaba <fabinhokb44@gmail.com>
2025-04-06 19:52:52 -0300

    feat: adicionar método de domínio para atualizar produto

--- src/main/java/com/fabiokusaba/vendas_api/domain/model/Produto.java
+++ src/main/java/com/fabiokusaba/vendas_api/domain/model/Produto.java
@@ -2,6 +2,7 @@ package com.fabiokusaba.vendas_api.domain.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.UUID;
@@ -33,6 +34,18 @@ public class Produto {
     public static Produto novoProduto(String nome, String descricao, List<String> categorias, String codigoDeBarras, Double preco, Integer quantidade) {
         final var id = UUID.randomUUID().toString();
         return new Produto(id, nome, descricao, categorias, codigoDeBarras, preco, quantidade, true);
+    }
+
+    public Produto atualizarProduto(String nome, String descricao, List<String> categorias, String codigoDeBarras, Double preco, Integer quantidade) {
+        if (StringUtils.hasText(codigoDeBarras)) {
+            this.codigoDeBarras = codigoDeBarras;
+        }
+        this.nome = nome;
+        this.descricao = descricao;
+        this.categorias = categorias;
+        this.preco = preco;
+        this.quantidade = quantidade;
+        return this;
     }
 
     public String getId() {
