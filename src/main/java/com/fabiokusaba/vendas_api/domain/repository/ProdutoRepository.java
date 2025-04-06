b9641b741e46665e59e9baf05256200c4a6cdbf9
Fabio Kusaba <fabinhokb44@gmail.com>
2025-04-06 18:22:23 -0300

    feat: adicionar query para buscar produto por nome ignorando case

--- src/main/java/com/fabiokusaba/vendas_api/domain/repository/ProdutoRepository.java
+++ src/main/java/com/fabiokusaba/vendas_api/domain/repository/ProdutoRepository.java
@@ -3,5 +3,9 @@ package com.fabiokusaba.vendas_api.domain.repository;
 import com.fabiokusaba.vendas_api.domain.model.Produto;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends MongoRepository<Produto, String> {
+
+    List<Produto> findByNomeContainingIgnoreCase(String nome);
 }
