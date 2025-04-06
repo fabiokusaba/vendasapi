f3bd32929967274027f0045a42e53d68dc5451a0
Fabio Kusaba <fabinhokb44@gmail.com>
2025-04-06 09:42:18 -0300

    refactor: alterar visibilidade do construtor do value object Endereco e criar método estático para instanciar novo objeto

--- src/main/java/com/fabiokusaba/vendas_api/vo/Endereco.java
+++ src/main/java/com/fabiokusaba/vendas_api/vo/Endereco.java
@@ -10,7 +10,7 @@ public class Endereco {
     private String complemento;
     private String numero;
 
-    public Endereco(String cep, String uf, String cidade, String bairro, String logradouro, String complemento, String numero) {
+    private Endereco(String cep, String uf, String cidade, String bairro, String logradouro, String complemento, String numero) {
         this.cep = cep;
         this.uf = uf;
         this.cidade = cidade;
@@ -18,6 +18,10 @@ public class Endereco {
         this.logradouro = logradouro;
         this.complemento = complemento;
         this.numero = numero;
+    }
+
+    public static Endereco novoEndereco(String cep, String uf, String cidade, String bairro, String logradouro, String complemento, String numero) {
+        return new Endereco(cep, uf, cidade, bairro, logradouro, complemento, numero);
     }
 
     public String getCep() {
