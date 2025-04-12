fa399b689816adaab4e16b231f239fe26131a2fa
Fabio Kusaba <fabinhokb44@gmail.com>
2025-04-12 19:05:55 -0300

    feat: criar método para converter request para entity

--- src/main/java/com/fabiokusaba/vendas_api/api/v1/request/venda/cadastrar/CadastrarVendaRequest.java
+++ src/main/java/com/fabiokusaba/vendas_api/api/v1/request/venda/cadastrar/CadastrarVendaRequest.java
@@ -1,6 +1,8 @@
 package com.fabiokusaba.vendas_api.api.v1.request.venda.cadastrar;
 
 import com.fabiokusaba.vendas_api.api.v1.request.venda.ItemVendaRequest;
+import com.fabiokusaba.vendas_api.domain.model.Venda;
+import com.fabiokusaba.vendas_api.domain.vo.ItemVenda;
 
 import java.util.List;
 
@@ -8,4 +10,19 @@ public record CadastrarVendaRequest(
         String clienteId,
         List<ItemVendaRequest> itens
 ) {
+
+    public static Venda toVenda(CadastrarVendaRequest request) {
+        return Venda.novaVenda(
+                request.clienteId(),
+                request.itens().stream()
+                        .map(item -> ItemVenda.novoItemVenda(
+                                item.produtoId(),
+                                null,
+                                null,
+                                item.quantidade(),
+                                null
+                        ))
+                        .toList()
+        );
+    }
 }
